927a86eaec105677bb0ced0a37c88056dc9089b0
Godlife21 <s11610104@student.unklab.ac.id>
2021-05-14 03:27:15 +0800

    buat page Repair Shop

---
+++ src/pages/Repair Shop/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const RepairShop = () => {
+    return ( <
+        View >
+        <
+        Text > Repair Shop < /Text> <
+        /View>
+    );
+};
+
+export default RepairShop;
+
+const styles = StyleSheet.create({});
