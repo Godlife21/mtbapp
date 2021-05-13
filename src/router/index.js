a7a0c5de005132b4fbc81cbe5346d97a5eb63638
Godlife21 <s11610104@student.unklab.ac.id>
2021-05-13 16:31:52 +0800

    tambah router untuk navigasi

---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Home, Start } from '../pages';
+
+const Stack = createStackNavigator();
+
+
+const Router = () => {
+    return ( <
+        Stack.Navigator >
+        <
+        Stack.Screen name = "Start"
+        component = { Start }
+        options = {
+            { headerShown: false } }
+        /> <
+        Stack.Screen name = "Home"
+        component = { Home }
+        options = {
+            { headerShown: false } }
+        /> <
+        /Stack.Navigator>
+    );
+};
+
+export default Router;
